b07c6ff45763893bf4bf37dc0ea427509b7148e1
meera-420 <mam1897@nyu.edu>
2022-05-05 20:55:17 +0400

    5

--- script.js
+++ script.js
@@ -42,3 +42,57 @@ jQuery(document).ready(function($){
     }, 350);
 
 });
+
+
+// fish moving
+
+//--  ast animation
+$(document).ready(function() {
+    animateDiv($('#fish1'));
+		animateDiv($('#fish2'));
+		animateDiv($('#fish3'));
+		animateDiv($('#fish4'));
+
+});
+// this creates the container for the images to float in
+function makeNewPosition($panel4) {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $panel4.height() - 50;
+    var w = $panel4.width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+// this creates the  movement
+function animateDiv($target) {
+    var newq = makeNewPosition($target.parent());
+    var oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+    $target.animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function() {
+        animateDiv($target);
+    });
+
+};
+// this is for speed
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
